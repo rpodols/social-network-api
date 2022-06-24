49237a935701abbdad5bb85e58d608762f591812
ryanp <rdpodols@gmail.com>
2022-06-24 18:33:52 -0400

    added routes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,36 @@
+const router = require('express').Router();
+
+const {
+    getAllThoughts,
+    createThought,
+    getThoughtById,
+    updateThought,
+    deleteThought,
+    createReaction,
+    removeReaction
+
+} = require('../../controllers/thought-controller');
+
+// /api/thoughts
+router
+    .route('/')
+    .get(getAllThoughts)
+    .post(createThought);
+
+// /api/thoughts/:id
+router
+    .route('/:id')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router
+    .route('/:id/reactions')
+    .post(createReaction)
+
+router
+    .route(':id/reactions/:reactionId')
+    .delete(removeReaction);
+
+module.exports = router;
---
+++ routes/api/user-routes.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+
+const {
+    getAllUsers,
+    createUser,
+    getUserById,
+    updateUser,
+    deleteUser,
+    addNewFriend,
+    deleteFriend
+
+} = require('../../controllers/user-controller');
+
+// /api/users           ** BONUS remove associated throughts when deleteing user **
+router
+    .route('/')
+    .get(getAllUsers)
+    .post(createUser);
+
+// /api/users/:id
+router 
+    .route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
+
+// /api/users/:userId/friends/:friendsId             
+router 
+    .router('/:id/friends/:friendsId')
+    .post(addNewFriend)
+    .delete(deleteFriend)
+
+module.exports = router;
